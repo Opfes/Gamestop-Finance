d63349675c8b2725d54ef37198cd3f56efc3ea92
nmgrovic <nmgrovic@go.olemiss.edu>
2021-03-30 01:43:40 -0500

    removed state for savings

--- src/Onboard.js
+++ src/Onboard.js
@@ -43,7 +43,6 @@ function Onboard(){
     
     const [val_401k, setVal_401k] = useState('');
     const [val_bonds, setVal_bonds] = useState('');
-    const [val_savings, setVal_savings] = useState('');
     const [dropdown_select, setdropdown_select] = useState('');
     const [error, setError] = useState(null);
 
