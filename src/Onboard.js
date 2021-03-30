24987e41ccbe93b3f265c99e8337a39c3ca08b84
dmlindsa <dmlindsa@go.olemiss.edu>
2021-03-30 01:10:24 -0500

    added label to dropdown

--- src/Onboard.js
+++ src/Onboard.js
@@ -120,7 +120,7 @@ function Onboard(){
                                     alignItems="center"
                                     spacing='2'
                                     >
-                                        <Grid item><Select variant='outlined' className={classes.textInputField} value={dropdown_select} name="dropdownSelectorVal"/></Grid>
+                                        <Grid item><Select variant='outlined' className={classes.textInputField} label="Type of Financial Data" value={dropdown_select} name="dropdownSelectorVal"/></Grid>
                                         <Grid item><TextField variant='outlined' className={classes.dataInputField} label="401k Value" value={val_401k} name="val401k" onChange = {(event) => onChangeHandler(event)} /></Grid>
                                         <Grid item><TextField variant='outlined' className={classes.dataInputField} label="Bonds Value" value={val_bonds} name="valbonds" onChange = {(event) => onChangeHandler(event)}/></Grid>
                                         <Grid item><TextField variant='outlined' className={classes.dataInputField} label="Savings Value" value={val_savings} name="valsavings" onChange = {(event) => onChangeHandler(event)}/></Grid>
