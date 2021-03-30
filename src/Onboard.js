fc0dd374e13e5a0f7c2a719c0d1db021927e766d
dmlindsa <dmlindsa@go.olemiss.edu>
2021-03-30 01:25:23 -0500

    added 401k option

--- src/Onboard.js
+++ src/Onboard.js
@@ -124,6 +124,7 @@ function Onboard(){
                                         <Grid item><Select variant='outlined' className={classes.dropdownTypeSelect} label="Type of Financial Data" value={dropdown_select} name="dropdownSelectorVal">
                                             <MenuItem value=''>None</MenuItem>
                                             <MenuItem value={"Equity"}>Equity</MenuItem>
+                                            <MenuItem value={"401k"}>401k</MenuItem>
                                         </Select></Grid>
                                         <Grid item><TextField variant='outlined' className={classes.dataInputField} label="401k Value" value={val_401k} name="val401k" onChange = {(event) => onChangeHandler(event)} /></Grid>
                                         <Grid item><TextField variant='outlined' className={classes.dataInputField} label="Bonds Value" value={val_bonds} name="valbonds" onChange = {(event) => onChangeHandler(event)}/></Grid>
