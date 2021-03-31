be6a7f48239fcc63a43ef26d3033bfadd4248116
William B. James <wbjames2@go.olemiss.edu>
2021-03-30 19:38:52 -0500

    removed bonds from generate

--- src/firebase.js
+++ src/firebase.js
@@ -22,7 +22,7 @@ export const signInWithGoogle = () => {
 auth.signInWithPopup(provider);
 };
 
-export const generateUserDocument = async (user, valuebonds, valuesavings, valueequity) => {
+export const generateUserDocument = async (user, valuesavings, valueequity) => {
   if (!user) return;
   const userRef = firestore.doc(`Users/${user.uid}`);
   const snapshot = await userRef.get();
