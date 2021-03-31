c6f9c891dcc15488e39fd4451a5b4a80f830ef06
William B. James <wbjames2@go.olemiss.edu>
2021-03-30 19:40:20 -0500

    removed equity from generate

--- src/firebase.js
+++ src/firebase.js
@@ -22,7 +22,7 @@ export const signInWithGoogle = () => {
 auth.signInWithPopup(provider);
 };
 
-export const generateUserDocument = async (user, valueequity) => {
+export const generateUserDocument = async (user) => {
   if (!user) return;
   const userRef = firestore.doc(`Users/${user.uid}`);
   const snapshot = await userRef.get();
