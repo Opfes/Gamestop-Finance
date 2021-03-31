ff3e9da4bdf11893e8ba594ccc5bc5cabb637bf8
William B. James <wbjames2@go.olemiss.edu>
2021-03-30 19:44:10 -0500

    removed bonds from generate function

--- src/firebase.js
+++ src/firebase.js
@@ -34,7 +34,6 @@ export const generateUserDocument = async (user, dropdown_select, userinput_val)
       await userRef.set({
         displayName,
         email,
-        valuebonds,
         valuesavings,
         valueequity,
       });
